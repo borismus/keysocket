9690dea8613e676e71f4ec4bf05e94f9378b2b40
Ian Duffy <ian@ianduffy.ie>
2013-08-19 21:36:30 +0100

    Add support for youtube

---
+++ extension/keysocket-youtube.js
@@ -0,0 +1,73 @@
+var PREV = 20;
+var PLAY = 16;
+var NEXT = 19;
+
+function simulateClick(selector) {
+  var evt = document.createEvent('MouseEvents');
+  evt.initMouseEvent('mousedown', true, false,  document, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+  document.querySelector(selector).dispatchEvent(evt);
+  evt = document.createEvent('MouseEvents');
+  evt.initMouseEvent('mouseup', true, false,  document, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+  document.querySelector(selector).dispatchEvent(evt);
+  evt = document.createEvent('MouseEvents');
+  evt.initMouseEvent('click', true, false,  document, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+  document.querySelector(selector).dispatchEvent(evt);
+}
+
+var connection = null;
+var isConnected = false;
+
+function connect() {
+  // Connect to a websocket server
+  connection = new WebSocket('ws://localhost:1337/');
+
+  // When the connection is open, send some data to the server
+  connection.onopen = function() {
+    console.log('WS open');
+    isConnected = true;
+    connection.send('Ping'); // Send the message 'Ping' to the server
+
+
+    // Log errors
+    connection.onerror = function(error) {
+      console.log('WS error', error);
+    };
+
+    // Log messages from the server
+    connection.onmessage = function(e) {
+      console.log('WS message', e);
+      var key = e.data;
+      if (key == NEXT) {
+        simulateClick('.yt-uix-button-icon-playlist-bar-next');
+      } else if (key === PLAY) {
+        var video = document.getElementById("movie_player");
+        if(video.getPlayerState() === 1) {
+          video.pauseVideo();
+        } else {
+          video.playVideo();
+        }
+      } else if (key === PREV) {
+        simulateClick('.yt-uix-button-icon-playlist-bar-prev');
+      }
+    };
+
+    connection.onclose = function(e) {
+      console.log('WS close', e);
+      isConnected = false;
+      reconnect();
+    };
+  };
+
+}
+
+function reconnect() {
+  // If we're not connected,
+  if (!isConnected) {
+    // Attempt to connect.
+    connect();
+    // Then ensure we're connected.
+    setTimeout(reconnect, 1000);
+  }
+}
+
+reconnect();
