1bebe71fa9153cbed56f69bc6b76a33bf92c80d5
Isaac Waller <isaac@siriusapplications.com>
2014-07-24 17:19:17 -0700

    Add Xbox Music

---
+++ extension/keysocket-xboxmusic.js
@@ -0,0 +1,14 @@
+function onKeyPress(key) {
+    if(key === NEXT) {
+        var nextButton = document.querySelector('.iconPlayerNext');
+        simulateClick(nextButton);
+    } else if(key === PLAY) {
+        var playPauseButton = document.querySelector('.iconPlayerPause') || document.querySelector('.iconPlayerPlay');
+        simulateClick(playPauseButton);
+    } else if(key === PREV) {
+        var backButton = document.querySelector('.iconPlayerPrevious');
+        simulateClick(backButton);
+    }
+}
+
+console.log("keysocket: Loading Xbox Music extension");
