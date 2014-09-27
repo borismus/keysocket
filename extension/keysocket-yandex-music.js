cd7d98c7eb11c5ac1d0a72001b988f59acef2278
kakysha <ezhikvdele@gmail.com>
2014-09-28 02:28:51 +0400

    Support old & new Ya.Music UI

--- extension/keysocket-yandex-music.js
+++ extension/keysocket-yandex-music.js
@@ -1,13 +1,14 @@
-var playTarget = '.player-controls__btn_play';
-var nextTarget = '.player-controls__btn_next';
-var prevTarget = '.player-controls__btn_prev';
+var playTarget = '.player-controls__btn_play, .b-jambox__play';
+var nextTarget = '.player-controls__btn_next, .b-jambox__next';
+var prevTarget = '.player-controls__btn_prev, .b-jambox__prev';
 
 function onKeyPress(key) {
+    // try to click both old and new interface buttons
     if (key === PREV) {
-        simulateClick(document.querySelector(prevTarget));
+        simulateClick(document.querySelector(playTarget));
     } else if (key === NEXT) {
         simulateClick(document.querySelector(nextTarget));
     } else if (key === PLAY) {
-        simulateClick(document.querySelector(playTarget));
+        simulateClick(document.querySelector(prevTarget));
     }
 }
