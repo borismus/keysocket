6c44532fe790c0bd5d9ea2ce70c6d954ced9d645
Matthew Dale <matthew.robert.dale@gmail.com>
2014-11-14 23:04:46 -0800

    Add keysocket control for listen.tidalhifi.com

---
+++ extension/keysocket-tidal.js
@@ -0,0 +1,21 @@
+function onKeyPress(key) {
+    if(key === NEXT) {
+        var nextButton = document.querySelector('button.play-controls__next');
+        simulateClick(nextButton);
+    } else if(key === PLAY) {
+        var playButton = document.querySelector('button.play-controls__play');
+        var pauseButton = document.querySelector('button.play-controls__pause');
+        if(isVisible(playButton)) {
+            simulateClick(playButton);
+        } else {
+            simulateClick(pauseButton)
+        }
+    } else if(key === PREV) {
+        var backButton = document.querySelector('button.play-controls__previous');
+        simulateClick(backButton);
+    }
+}
+
+function isVisible(el) {
+    return el.offsetParent != null;
+}
