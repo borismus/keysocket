e317d2a8e5581ec3f845636432849b650447d46c
Eric Nemchik <eric@nemchik.com>
2015-03-05 12:18:35 -0600

    Prefer ' over "

--- extension/background.js
+++ extension/background.js
@@ -10,13 +10,13 @@ chrome.commands.onCommand.addListener(function(command) {
 
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
-		console.log("Received tab message: ", request);
-		if(request.command == "activateTab" && sender.tab) {
+		console.log('Received tab message: ', request);
+		if(request.command == 'activateTab' && sender.tab) {
 			if(activeTabs.indexOf(sender.tab.id) == -1) {
 				activeTabs.push(sender.tab.id);
 			}
 				chrome.pageAction.show(sender.tab.id);
-				chrome.pageAction.setTitle({tabId: sender.tab.id, title: "Click to disable media keys for this tab"});
+				chrome.pageAction.setTitle({tabId: sender.tab.id, title: 'Click to disable media keys for this tab'});
 		}
 	}
 );
@@ -32,11 +32,11 @@ chrome.pageAction.onClicked.addListener(function(tab) {
 	var index = activeTabs.indexOf(tab.id);
 	if(index < 0) {
 		activeTabs.push(tab.id);
-		chrome.pageAction.setIcon({tabId: tab.id, path: {"19": "icons/icon19.png", "38": "icons/icon38.png"}});
-		chrome.pageAction.setTitle({tabId: tab.id, title: "Click to disable media keys for this tab"});
+		chrome.pageAction.setIcon({tabId: tab.id, path: {'19': 'icons/icon19.png', '38': 'icons/icon38.png'}});
+		chrome.pageAction.setTitle({tabId: tab.id, title: 'Click to disable media keys for this tab'});
 	} else {
 		activeTabs.splice(index, 1);
-		chrome.pageAction.setIcon({tabId: tab.id, path: {"19": "icons/icon19-inactive.png", "38": "icons/icon38-inactive.png"}});
-		chrome.pageAction.setTitle({tabId: tab.id, title: "Click to enable media keys for this tab"});
+		chrome.pageAction.setIcon({tabId: tab.id, path: {'19': 'icons/icon19-inactive.png', '38': 'icons/icon38-inactive.png'}});
+		chrome.pageAction.setTitle({tabId: tab.id, title: 'Click to enable media keys for this tab'});
 	}
 });
