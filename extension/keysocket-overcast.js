40f9b2fa551b42c55207678cb5c7f9e02a2a0b24
Eric Nemchik <eric@nemchik.com>
2015-03-05 12:36:22 -0600

    Fixed a few issues where code had been moved around incorrectly

--- extension/keysocket-overcast.js
+++ extension/keysocket-overcast.js
@@ -1,14 +1,12 @@
 function onKeyPress(key) {
-    var player = document.getElementById('audioplayer');
-    if(key === PLAY) {
-        if(!player.paused) {
-            player.pause();
-        } else {
-            player.play();
-        }
-    } else if(key === STOP) {
-        if(!player.paused) {
-            player.pause();
-        }
+  var player = document.getElementById('audioplayer');
+  if(key === PLAY) {
+    if(!player.paused) {
+      player.pause();
+    } else {
+      player.play();
     }
+  } else if(key === STOP) {
+    player.pause();
+  }
 }
--- extension/keysocket-songza.js
+++ extension/keysocket-songza.js
@@ -1,7 +1,7 @@
 function onKeyPress(key) {
-    if(key === NEXT) {
-        simulateClick(document.querySelector('#player .miniplayer-control-skip'));
-    } else if(key === PLAY) {
-        simulateClick(document.querySelector('#player .miniplayer-control-play-pause'));
-    }
+  if(key === NEXT) {
+    simulateClick(document.querySelector('#player .miniplayer-control-skip'));
+  } else if(key === PLAY) {
+    simulateClick(document.querySelector('#player .miniplayer-control-play-pause'));
+  }
 }
