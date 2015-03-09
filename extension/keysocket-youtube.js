17c54faf2de89a05f41bf706c2c1a2bfdb3e2267
unknown <enemchik@enemchik-PC.Keeforce.local>
2015-03-09 10:09:34 -0500

    Update YouTube socket

--- extension/keysocket-youtube.js
+++ extension/keysocket-youtube.js
@@ -1,31 +1,23 @@
 function onKeyPress(key) {
-    if (key === NEXT) {
-        var nextButton = document
-            .querySelector('.yt-uix-button-icon-playlist-bar-next,.yt-uix-button-icon-watch-appbar-play-next');
-        simulateClick(nextButton);
-    } else if (key === PLAY) {
-        var video = document.querySelector('#movie_player');
-
-        // The extension has no access to this function when using the HTML-5 <video>-player
-        // but we can still access the controls on the <video>-element itself
-        if (!video.getPlayerState) {
-            video = document.querySelector('#movie_player video'); // HTML-5 <video>-player
-
-            if (video.paused) {
-                video.play();
-            } else {
-                video.pause();
-            }
-        } else {
+    var video = document.querySelector('#movie_player');
+    if (!video.getPlayerState) { // HTML5
+        if (key === NEXT) {
+            var nextButton = document.querySelector('.ytp-button-next');
+            simulateClick(nextButton);
+        } else if (key === PLAY) {
+            var playPauseButton = document.querySelector('.ytp-button-pause') || document.querySelector('.ytp-button-play');
+            simulateClick(playPauseButton);
+        } else if (key === PREV) {
+            var backButton = document.querySelector('.ytp-button-prev');
+            simulateClick(backButton);
+        }
+    } else { // Flash
+        if (key === PLAY) {
             if (video.getPlayerState() === 2) {
                 video.playVideo();
             } else {
                 video.pauseVideo();
             }
         }
-    } else if (key === PREV) {
-        var backButton = document
-            .querySelector('.yt-uix-button-icon-playlist-bar-prev,.yt-uix-button-icon-watch-appbar-play-prev');
-        simulateClick(backButton);
     }
 }
