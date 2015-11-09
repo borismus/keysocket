d2642382cc812c1da7ee3b52823156377e2da767
ALiangLiang <david27072183@gmail.com>
2015-11-10 04:05:12 +0800

    Improve selector

--- extension/keysocket-subsonic.js
+++ extension/keysocket-subsonic.js
@@ -1,13 +1,13 @@
 function onKeyPress(key) {
-	if (this.jwplayer) {
-		if (key === NEXT) {
-			var playPauseButton = document.querySelector('img[src="icons/default_dark/forward.png"]');
-			simulateClick(playPauseButton);
-		} else if (key === PLAY) {
+  if (this.jwplayer) {
+    if (key === NEXT) {
+      var playPauseButton = document.querySelector('img[onclick="onNext(false)"]');
+      simulateClick(playPauseButton);
+    } else if (key === PLAY) {
       location.href = "javascript:window.jwplayer().play()";
-		} else if (key === PREV) {
-			var backButton = document.querySelector('img[src="icons/default_dark/back.png"]');
-			simulateClick(backButton);
-		}
-	}
+    } else if (key === PREV) {
+      var backButton = document.querySelector('img[onclick="onPrevious()"]');
+      simulateClick(backButton);
+    }
+  }
 }
