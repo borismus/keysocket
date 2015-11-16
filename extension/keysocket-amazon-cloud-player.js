58d4b4fef91f99730ced718f4a877c71ce2b9180
Fiete Börner <fieteboerner93@googlemail.com>
2015-11-16 21:24:14 +0100

    update amazon cloudplayer functionallity 	pattern now also matches amazon.de and new cloudplayer url 	update new query selectors

--- extension/keysocket-amazon-cloud-player.js
+++ extension/keysocket-amazon-cloud-player.js
@@ -1,12 +1,12 @@
 function onKeyPress(key) {
     if (key === NEXT) {
-        var nextButton = document.querySelector('[playeraction=next]');
+        var nextButton = document.querySelector('.nextButton');
         simulateClick(nextButton);
     } else if (key === PLAY) {
-        var playPauseButton = document.querySelector('[playeraction=togglePlay]');
+        var playPauseButton = document.querySelector('.playButton');
         simulateClick(playPauseButton);
     } else if (key === PREV) {
-        var backButton = document.querySelector('[playeraction=previous]');
+        var backButton = document.querySelector('.previousButton');
         simulateClick(backButton);
     }
 }
