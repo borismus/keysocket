6e212b571d26742b7764b06a61e164f4c08f855b
Chuck Holbrook <chuck@streetdeck.com>
2016-02-28 21:33:37 -0800

    Added support for siriusXM

--- extension/keysocket-siriusxm.js
+++ extension/keysocket-siriusxm.js
@@ -1,23 +1,40 @@
 function onKeyPress(key) {
-    var video = document.querySelector('#movie_player');
-    if (!video.getPlayerState) { // HTML5 Player
-        if (key === NEXT) {
-            var nextButton = document.querySelector('.ytp-button-next, .ytp-next-button');
-            simulateClick(nextButton);
-        } else if (key === PLAY) {
-            var playPauseButton = document.querySelector('.ytp-button-pause, .ytp-button-play, .ytp-pause-button, .ytp-play-button');
-            simulateClick(playPauseButton);
-        } else if (key === PREV) {
-            var backButton = document.querySelector('.ytp-button-prev, .ytp-prev-button');
-            simulateClick(backButton);
-        }
-    } else { // Flash Player
-        if (key === PLAY) {
-            if (video.getPlayerState() === 2) {
-                video.playVideo();
-            } else {
-                video.pauseVideo();
+    /**
+     * Look for the play dom button to check if its playing
+     * @returns {boolean} Returns true if the dom element was found and not hidden with ng-hide
+     * @private
+      */
+    function _isPlaying(){
+        var playButtonElement = document.querySelector('.scrub-controls [ng-click^=play]');
+        if (playButtonElement) {
+            for (var i = 0; i < playButtonElement.classList.length; i++) {
+                var className = playButtonElement.classList[i];
+                if (className === 'ng-hide') {
+                    return false;
+                }
             }
+
+            return true;
         }
+
+        return false;
     }
+
+    if (key === NEXT) {
+        var nextButton = document.querySelector('.scrub-controls [ng-click^=skip]');
+        simulateClick(nextButton);
+    } else if (key === PLAY) {
+
+        var buttonEl;
+        if (_isPlaying()) {
+            buttonEl = document.querySelector('.scrub-controls [ng-click^=play]');
+        } else {
+            buttonEl = document.querySelector('.scrub-controls [ng-click^=pause]');
+        }
+        simulateClick(buttonEl);
+    } else if (key === PREV) {
+        var backButton = document.querySelector('.scrub-controls [ng-click^=rewind]');
+        simulateClick(backButton);
+    }
+
 }
