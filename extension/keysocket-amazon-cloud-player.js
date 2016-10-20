a79e5dc13220c1628e3b7d692f152407a8d5ab33
raztus <raztus@gmail.com>
2016-10-20 12:18:54 -0600

    Fix amazon cloud player

--- extension/keysocket-amazon-cloud-player.js
+++ extension/keysocket-amazon-cloud-player.js
@@ -1,12 +1,12 @@
 function onKeyPress(key) {
     if (key === NEXT) {
-        var nextButton = document.querySelector('.button.icon-fastForward');
+        var nextButton = document.querySelector('#dragonflyTransport .button.icon-fastForward');
         simulateClick(nextButton);
     } else if (key === PLAY) {
-        var playPauseButton = document.querySelector(".button.playButton");
+        var playPauseButton = document.querySelector("#dragonflyTransport .button.playButton");
         simulateClick(playPauseButton);
     } else if (key === PREV) {
-        var backButton = document.querySelector('.button.icon-fastBackward');
+        var backButton = document.querySelector('#dragonflyTransport .button.icon-fastBackward');
         simulateClick(backButton);
     }
 }
