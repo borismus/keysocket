c789c105c8bfdd6b706542e84de5e79fc8c29333
Kristian Johannessen <kristian.johannessen@tibe.no>
2017-05-10 14:50:10 +0200

    use shared injection functionality

--- extension/keysocket-nrkradio.js
+++ extension/keysocket-nrkradio.js
@@ -3,44 +3,8 @@
  * so we have to talk to an api in the browser window to get media buttons working.
  */
 
-
-/* Runs function in actual window */
-function injectScript(injectFunction) {
-    var script = document.createElement('script');
-    script.textContent = '(' + injectFunction + ')();';
-    (document.head||document.documentElement).appendChild(script);
-    script.parentNode.removeChild(script);
-}
-
-/* Call toggleplay in nrk flash API */
-function nrkToggleplay() {
-    injectScript(function() {
+injectionInit(function (key) {
+    if (key === "play-pause") {
         nrk.modules.player.getApi().toggleplay();
-    });
-}
-
-/* Seek forward */
-function nrkSeekForward() {
-    injectScript(function() {
-        var api = nrk.modules.player.getApi();
-        api.seek(api.getPosition()+30);
-    });
-}
-
-/* Seek backward */
-function nrkSeekBackward() {
-    injectScript(function() {
-        var api = nrk.modules.player.getApi();
-        api.seek(api.getPosition()-30);
-    });
-}
-
-function onKeyPress(key) {
-    if (key === PLAY) {
-        nrkToggleplay();
-    } else if (key === PREV) {
-        nrkSeekBackward();
-    } else if (key === NEXT) {
-        nrkSeekForward();
-    }    
-}
+    }
+});
