effe7dc54673ffbcd22ab8a16f4b43fc8ba199b4
Alf Storm <astorm@nevion.com>
2017-07-28 13:06:30 +0200

    Changed nrkradio to use CSS selector instead of API call.

--- extension/keysocket-nrkradio.js
+++ extension/keysocket-nrkradio.js
@@ -1,10 +1,6 @@
-/*
- * NRK radio only supports a flash player on chrome desktop at the moment,
- * so we have to talk to an api in the browser window to get media buttons working.
- */
-
-injectionInit(function (key) {
-    if (key === "play-pause") {
-        nrk.modules.player.getApi().toggleplay();
+function onKeyPress(key) {
+    if (key === PLAY) {
+        var playPauseButton = document.querySelector('button.ludo-bar__button--playpause');
+        simulateClick(playPauseButton);
     }
-});
+}
