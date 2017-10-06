a0994503d74062d2fa02a26929ec49f99214cafe
Emad Aldeen Hajjar <emadaldeen.hajjar@gmail.com>
2017-10-05 23:10:42 -0300

    bugfix: update to new API

--- extension/keysocket-crunchyroll.js
+++ extension/keysocket-crunchyroll.js
@@ -1,12 +1,19 @@
-function onKeyPress(key) {
-    var video = document.querySelector('#showmedia_video_player');
-    if (key === NEXT) {
-        location.href = document.querySelector("link[rel=next]").getAttribute("href");
-    } else if (key === PREV) {
-        location.href = document.querySelector("link[rel=prev]").getAttribute("href");
-    } if (key === PLAY) {
-        video.togglePlayPause();
-    } else if (key === STOP) {
-        video.pauseVideo();
+keySocket.init(
+    "crunchyroll",
+    {
+        "play-pause": function () {
+            var video = document.querySelector('#showmedia_video_player');
+            video.togglePlayPause();
+        },
+        "prev": function () {
+            location.href = document.querySelector("link[rel=prev]").getAttribute("href");
+        },
+        "next": function () {
+            location.href = document.querySelector("link[rel=next]").getAttribute("href");
+        },
+        "stop": function () {
+            var video = document.querySelector('#showmedia_video_player');
+            video.pauseVideo();
+        },
     }
-}
+);
--- extension/keysocket-vimeo.js
+++ extension/keysocket-vimeo.js
@@ -1,13 +1,15 @@
-function onKeyPress(key) {
-    var video = document.querySelector('#showmedia_video_player');
-    if (key === NEXT) {
-        location.href = document.querySelector("a[rel=next]").getAttribute("href");
-    } else if (key === PREV) {
-        location.href = document.querySelector("a[rel=prev]").getAttribute("href");
-    } if (key === PLAY) {
-        var playPauseButton = document.querySelector(".controls button");
-        keySocket.simulateClick(playPauseButton);
-    } else if (key === STOP) {
-        location.href = document.querySelector("link[rel=canonical]").getAttribute("href");
+keySocket.init(
+    "vimeo",
+    {
+        "play-pause": '.controls button',
+        "prev": function () {
+            location.href = document.querySelector("a[rel=prev]").getAttribute("href");
+        },
+        "next": function () {
+            location.href = document.querySelector("a[rel=next]").getAttribute("href");
+        },
+        "stop": function () {
+            location.href = document.querySelector("link[rel=canonical]").getAttribute("href");
+        },
     }
-}
+);
