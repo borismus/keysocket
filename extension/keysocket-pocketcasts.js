a12de33a59b64dc347c60aac8b9bef011a604c9e
Mike Kelly <pioto@pioto.org>
2017-10-13 10:07:49 -0400

    Add support for the new beta of Pocket Casts
    Tested against: Version 2.0 Build 22
    
    Fixes: #243

--- extension/keysocket-pocketcasts.js
+++ extension/keysocket-pocketcasts.js
@@ -1,9 +1,9 @@
 keySocket.init(
     "pocketcasts",
     {
-        "play-pause": "div.play_pause_button",
-        "prev": "div.skip_back_button",
-        "next": "div.skip_forward_button"
+        "play-pause": "div.play_pause_button, div.animated-play-button",
+        "prev": "div.skip_back_button, div.skip-back-button",
+        "next": "div.skip_forward_button, div.skip-forward-button"
         // stop is omitted
     }
 );
