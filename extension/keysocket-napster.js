16a79cec8d0a4d59ce4d7259b5946b4c184b7c8f
Mathis Dupuy <mdupuy@eurodns.com>
2018-01-09 10:27:44 +0100

    Add support for app.napster.com

---
+++ extension/keysocket-napster.js
@@ -0,0 +1,9 @@
+keySocket.init(
+    "napster",
+    {
+        "play-pause": ".player-play-button > .icon-play-button, .player-play-button > .icon-pause2",
+        "prev": ".player-rewind-button.icon-previous2",
+        "next": ".player-advance-button.icon-next2"
+        // stop is omitted
+    }
+);
