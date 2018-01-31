076e52de78f149d09c23743afb321eba2cdc8894
Minh Nguyen <minh@misfit.com>
2018-01-31 18:28:39 +0700

    Add support for mp3.zing

---
+++ extension/keysocket-zingmp3.js
@@ -0,0 +1,9 @@
+keySocket.init(
+    "zing-mp3-player",
+    {
+        "play-pause": ".media-control-button.paused",
+        "prev": ".media-control-button.zp-button-prev",
+        "next": ".media-control-button.zp-button-next"
+        // stop is omitted
+    }
+);
