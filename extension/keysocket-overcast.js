369f2be8783772291e297935537e4b6bd98b2dbe
Nick Alexander <nalexander@medonesystems.com>
2018-05-17 15:50:12 -0400

    Use simplified Keysocket API

--- extension/keysocket-overcast.js
+++ extension/keysocket-overcast.js
@@ -1,12 +1,7 @@
-keySocket.init("overcast.fm", function (key) {
-    if (key === keySocket.NEXT) {
-        var seekForwardButton = document.getElementById("seekforwardbutton");
-        keySocket.simulateClick(seekForwardButton);
-    } else if (key === keySocket.PLAY) {
-        var playPauseButton = document.getElementById("playpausebutton");
-        keySocket.simulateClick(playPauseButton);
-    } else if (key === keySocket.PREV) {
-        var seekBackButton = document.getElementById("seekbackbutton");
-        keySocket.simulateClick(seekBackButton);
+keySocket.init("overcast.fm", {
+        "prev": "#seekbackbutton",
+        "play-pause": "#playpausebutton",
+        "next": "#seekforwardbutton"
+        // stop is omitted
     }
-});
+);
