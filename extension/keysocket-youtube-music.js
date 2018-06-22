77cb15ac31b7ceeb27c509556d0df8a164b655fb
Richard Leggett <richard@theappnest.co.uk>
2018-06-22 14:59:51 +0100

    Add YouTube Music support

---
+++ extension/keysocket-youtube-music.js
@@ -0,0 +1,9 @@
+keySocket.init(
+    "music.youtube",
+    {
+        "play-pause": ".play-pause-button",
+        "prev": '.previous-button',
+        "next": '.next-button'
+        // stop is omitted
+    }
+);
