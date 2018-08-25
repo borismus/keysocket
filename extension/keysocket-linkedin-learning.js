e5d57a821d3cb196339c1b1ee8bfb9a93192fead
Christian Oliff <christianoliff@yahoo.com>
2018-08-25 14:32:09 +0900

    Create keysocket-linkedin-learning.js

---
+++ extension/keysocket-linkedin-learning.js
@@ -0,0 +1,9 @@
+keySocket.init(
+    "linkedin",
+    {
+        "play-pause": ".ssplayer-play-button",
+        "prev": ".ssplayer-previous-button",
+        "next": ".ssplayer-next-button"
+        // stop is omitted
+    }
+);
