6e178907c783024c3402da0ff3cc4123c1b7e587
Patrick Kohan <patrick.kohan@gmail.com>
2019-02-04 16:19:37 +0100

    add play-pause for noilsi.com

---
+++ extension/keysocket-noisli.js
@@ -0,0 +1,14 @@
+keySocket.init(
+	"noisli",
+	{
+		"play-pause": function () {
+			var muteButton = document.querySelector("#sound-button-out")
+			var unmuteButton = document.querySelector("#unmute-button-out")
+			if (muteButton.style.display === 'none') {
+				unmuteButton.click();
+			} else {
+				muteButton.click();
+			}
+		}
+	}
+);
