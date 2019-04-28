6adca4c849fd33ee301db1b118ba6e557956a550
TLuigi003 <tluigi003.l3@gmail.com>
2019-04-28 17:51:43 +0200

    - Added rainwave support

---
+++ extension/keysocket-rainwave.js
@@ -0,0 +1,9 @@
+keySocket.init(
+    "rainwave",
+    {
+        "play-pause": ".audio_icon_play",
+        // "prev": ".inline_player .prevbutton",
+        // "next": ".inline_player .nextbutton"
+        "stop": ".audio_icon_stop"
+    }
+);
