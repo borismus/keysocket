c1a96522aaea7d912e10eeb836697f90386d727a
Victor <vyang@umich.edu>
2020-12-16 20:01:22 -0800

    fixed Spotify media controls

--- extension/keysocket-spotify.js
+++ extension/keysocket-spotify.js
@@ -1,9 +1,25 @@
 keySocket.init(
     "spotify",
-    {
-        "play-pause": ".spoticon-pause-16, .spoticon-play-16",
-        "prev": ".spoticon-skip-back-16",
-        "next": ".spoticon-skip-forward-16",
-        "stop": ".spoticon-pause-16"
+    function (key) {
+        if (key === keySocket.NEXT) {
+            var nextButton = document.querySelector(`[data-testid="control-button-skip-forward"]`);
+            nextButton.click();
+        } else if (key === keySocket.PLAY) {
+			var playButton = document.querySelector(`[data-testid="control-button-play"]`);
+            var pauseButton = document.querySelector(`[data-testid="control-button-pause"]`);
+			if (playButton === null) {
+				pauseButton.click();
+			} else if (pauseButton === null) {
+				playButton.click();
+			} else {
+				console.log("Could not find play or pause buttons..");
+			}
+        } else if (key === keySocket.PREV) {
+            var backButton = document.querySelector(`[aria-label="Previous"]`);
+            backButton.click();
+        } else if (key === keySocket.STOP) {
+            var stopButton = document.querySelector(`[data-testid="control-button-pause"]`);
+            stopButton.click();
+        }
     }
 );
