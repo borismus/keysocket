5d4a3350ce56b755a1fc1094506fdfc7875cfb8b
Christian Lutz <chlutz214@gmail.com>
2023-09-02 16:09:10 -0400

    Added Support for Internet Archive Live Music

---
+++ extension/keysocket-internet-archive.js
@@ -0,0 +1,9 @@
+keySocket.init(
+    "internetarchive",
+    {
+        "play-pause": ".jw-icon-playback",
+        "prev": "[button=btn-prv]",
+        "next": "[button=btn-nxt]",
+        // stop is omitted
+    }
+);
